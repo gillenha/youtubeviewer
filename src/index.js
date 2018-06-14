c778b9177993e7b1ed5985f3e33e89941dda3fa6
gillenha <gillen08@gmail.com>
2018-06-14 19:07:19 -0400

    setting up components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Hello from './Hello';
+// import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+const API_KEY = "AIzaSyDy2RXLjdGrcDg5EILjrlyTUEPc2ZdYnl0";
+
+const App = () => {
+	return <div className="f1 tc">Hi!</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
